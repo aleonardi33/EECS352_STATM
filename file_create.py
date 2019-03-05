cdb76f9d25fbc3397501ba3e25d0e7f7b50ca3d8
aleonardi33 <anthonyleonardi2020@u.northwestern.edu>
2019-03-05 16:45:13 -0600

    update file create

--- file_create.py
+++ file_create.py
@@ -3,13 +3,14 @@ import numpy as np
 ##input should be an n by 4 numpy array with n being each note and input[n,0]=note
 ##input[n,1]=velocity, input[n,2]=starttime, imput[n,3]=endtime
 ##the second input is the name of the ouput file as a string
-def file_create(notes,nof):
+def midi_file_create(notes,nof):
     mid = MidiFile()
     track = MidiTrack()
     mid.tracks.append(track)
     track.append(Message('program_change',program=0,time=0))
     num_notes = notes.shape[0]
     check = notes.shape[1]
+    print(notes)
     if check != 4:
         raise Exception('Height of Array does not equal 3')
     else:
@@ -20,7 +21,11 @@ def file_create(notes,nof):
                 if notes[i,1]>127:
                     raise Exception('Velocity not in range[0,127]')
                 else:
-                    track.append(Message('note_on',note=notes[i,0],velocity=notes[i,1],time=notes[i,2]))
-                    track.append(Message('note_off',note=notes[i,0],velocity=notes[i,1],time=notes[i,3]))
-        mid.save(filename=nof)
+                    track.append(Message('note_on',note=int(notes[i,0]),velocity=int(notes[i,1]),time=int(notes[i,2])))
+                    track.append(Message('note_off',note=int(notes[i,0]),velocity=int(notes[i,1]),time=int(notes[i,3])))
+        if nof.endswith('.mid'):
+            mid.save(filename=nof)
+        else:
+            mid.save(filename=nof+'.mid')
+
 
