f8bf09695c937cbdf7538be47c82389525e50ffd
aleonardi33 <anthonyleonardi2020@u.northwestern.edu>
2019-03-06 13:48:45 -0600

    translated from jupyter

---
+++ stft.py
@@ -0,0 +1,46 @@
+from scipy.fftpack import fft
+from scipy.signal.windows import hann
+import scipy as sp
+import numpy as np
+
+def stft(signal, window_size, hop_size, window_type = 'hann'):
+    """
+    Computes the short term fourier transform of a 1-D numpy array, where the array 
+    is windowed into a set of subarrays, each of length window_size. The distance between
+    window centers (in samples) is given by hop_size. The type of window applied is
+    determined by window_type. This returns a 2-D numpy array where the ith column
+    is the FFT of the ith window. Each column contains an array of complex values.
+    
+    Input Parameters
+    ----------------
+    signal: The 1-d (complex or real) numpy array containing the signal
+    window_size: an integer scalar specifying the number of samples in a window
+    hop_size: an integer specifying the number of samples between the start of adjacent windows
+    window_type: a string specifying one of two "hann" or "rectangular"
+    
+    Returns
+    -------
+    a 2D numpy array of complex numbers where the array column is the FFT of the ith window,
+    and the jth element in the ith column is the jth frequency of analysis.
+    """
+    
+    # figure out how many hops
+    length_to_cover_with_hops = len(signal) - window_size
+    assert (length_to_cover_with_hops >= 0), "window_size cannot be longer than the signal to be windowed"
+    num_hops = int(1 + np.floor(length_to_cover_with_hops/hop_size))
+    
+    # make our window function
+    if (window_type == 'hann'):
+        window = sp.signal.windows.hann(window_size, sym=False)
+    else:
+        window = np.ones(window_size) # rectangular window
+    
+    stft = [0]*num_hops
+    # fill the array with values 
+    for hop in range(num_hops):
+        start = hop*hop_size
+        end = start + window_size
+        unwindowed_sound = signal[start:end]
+        windowed_sound =  unwindowed_sound * window
+        stft[hop]= fft(windowed_sound, window_size) 
+    return np.array(stft).T
